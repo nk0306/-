b4419649148d4e7d1d5c763d70ca62aa88b5cb12
EC2 Default User <ec2-user@ip-172-31-45-165.ap-northeast-1.compute.internal>
2021-04-11 02:05:19 +0000

    ツイッター風はじめ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_224903) do
+ActiveRecord::Schema.define(version: 2021_04_11_015601) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
