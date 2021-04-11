a97c4971d0c1b6474a53eb8760e75fe019ab5da0
EC2 Default User <ec2-user@ip-172-31-45-165.ap-northeast-1.compute.internal>
2021-04-11 01:20:58 +0000

    signin error

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # get 'users/show'
   # devise_for :users
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  # root to: "home#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }
-get "users/show" => "users#show"
+  
+  root to: "home#index"
 end
