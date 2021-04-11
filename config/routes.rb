19892725d14f47a6d07e69f6a31d8a4b89693217
EC2 Default User <ec2-user@ip-172-31-45-165.ap-northeast-1.compute.internal>
2021-04-11 01:41:05 +0000

    プロフィール制作

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # get 'users/show'
-  # devise_for :users
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  
-  root to: "home#index"
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "users/show" => "users#show"
+ root to: "home#index"
 end
