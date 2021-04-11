7c03f5c596d0604fc02a3d73726b69cb80b92b9a
EC2 Default User <ec2-user@ip-172-31-45-165.ap-northeast-1.compute.internal>
2021-04-11 01:47:01 +0000

    編集機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     # サインアップ時にnameのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:character,:rank])
     # アカウント編集の時にnameとprofileのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :character, :rank])
   end
 
 end
